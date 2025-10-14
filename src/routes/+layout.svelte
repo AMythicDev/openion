300ec2a5d63fae3b016be5ac5a306b776943ead5
Arijit Dey <arijit79@protonmail.com>
2025-10-15 03:42:29 +0530

    feat: add the home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav class="mb-6 flex h-14 items-center justify-between bg-white px-12 shadow-sm">
+	<a
+		class="bg-linear-to-r from-primary to-secondary bg-clip-text font-brand text-3xl text-transparent"
+		href="/"
+	>
+		Openion
+	</a>
+	<div class="flex gap-3">
+		<div
+			class="flex h-10 items-center justify-center rounded-xl bg-gradient-to-r from-primary to-secondary p-0.5"
+		>
+			<a
+				href="/login"
+				class="flex h-full w-full items-center justify-center rounded-xl bg-white px-3 transition-colors outline-none hover:bg-gradient-to-r hover:from-primary hover:to-secondary hover:text-white"
+			>
+				Login
+			</a>
+		</div>
+		<div
+			class="flex h-10 items-center justify-center rounded-xl bg-gradient-to-r from-primary to-secondary p-0.5"
+		>
+			<a
+				href="/signup"
+				class="flex h-full w-full items-center justify-center rounded-xl bg-gradient-to-r from-primary to-secondary px-3 text-white transition-colors outline-none hover:bg-none hover:bg-white hover:text-black"
+			>
+				Sign Up
+			</a>
+		</div>
+	</div>
+</nav>
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<div class="flex flex-col items-center">
+	<main class="flex max-w-1/2 flex-col items-center rounded-md bg-white p-8 shadow-sm">
+		<h1 class="mb-4 w-full text-2xl font-bold">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+		</h1>
+		<div class="mb-4 leading-relaxed">
+			<p class="mb-1.5">
+				Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+				pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu
+				aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
+				Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class
+				aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+				pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu
+				aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
+				Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class
+				aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+			</p>
+		</div>
+		<div class="mb-2 w-full">
+			<h2 class="text-xl font-semibold">Comments</h2>
+		</div>
+		<div
+			class="w-full rounded-xl bg-gradient-to-r from-primary to-secondary p-0.5"
+		>
+			<form class="w-full max-h-10 min-h-10 flex flex-col items-center overflow-y-hidden transition-all focus-within:min-h-45 rounded-xl">
+				<textarea
+					class="min-h-10 w-full flex-1 bg-white px-2 transition-all outline-none placeholder:text-primary"
+					placeholder="Share your opinions"
+				></textarea>
+				<div class="flex w-full justify-end gap-2 bg-white p-2" tabindex="-1">
+					<button class="rounded-xl border p-2" tabindex="0">Cancel</button>
+					<button class="rounded-xl bg-secondary px-4" tabindex="0">Comment</button>
+				</div>
+			</form>
+		</div>
+	</main>
+</div>

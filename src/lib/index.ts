06d3e866113659a51fad1dfa6da91ae14e5a98da
Arijit Dey <arijit79@protonmail.com>
2025-10-18 23:11:37 +0530

    fix: new api url

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-export const API_URL = "http://localhost:8080"
+export const API_URL = "https://openion.onrender.com/"
 
 export function shortenName(name: string): string {
   name = name.trim();
